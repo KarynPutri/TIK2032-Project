32bee1b31314ad4e944b234a31ee15c6f81b8153
KarynPutri <karynputri026@student.unsrat.ac.id>
2025-06-02 01:46:44 +0800

    Update and rename blog.html to blog.php

--- blog.php
+++ blog.php
@@ -10,10 +10,10 @@
     <h1>Blog</h1>
     <hr>
     <div class="nav-links">
-        <a href="index.html">Home</a>
-        <a href="gallery.html">Gallery</a>
-        <a href="blog.html">Blog</a>
-        <a href="contact.html">Contact</a>
+        <a href="index.php">Home</a>
+        <a href="gallery.php">Gallery</a>
+        <a href="blog.php">Blog</a>
+        <a href="contact.php">Contact</a>
     </div>
     <hr>
 
@@ -22,21 +22,21 @@
             <img src="Images/article1.jpg" alt="Teknologi dalam Pendidikan">
             <h2>Teknologi dalam Pendidikan</h2>
             <p>Di era digital, teknologi telah secara signifikan mengubah dunia pendidikan...</p>
-            <a href="article1.html" class="read-more-button">Baca Selengkapnya</a>
+            <a href="article1.php" class="read-more-button">Baca Selengkapnya</a>
         </div>
 
         <div class="blog-post">
             <img src="Images/article2.jpg" alt="Pengaruh Mobile Legends">
             <h2>Pengaruh Game Mobile Legends</h2>
             <p>Mobile Legends: Bang Bang (MLBB) adalah salah satu game mobile paling populer...</p>
-            <a href="article2.html" class="read-more-button">Baca Selengkapnya</a>
+            <a href="article2.php" class="read-more-button">Baca Selengkapnya</a>
         </div>
 
         <div class="blog-post">
             <img src="Images/article3.jpeg" alt="Media Sosial">
             <h2>Media Sosial: Menghubungkan atau Justru Menjauhkan?</h2>
             <p>Media sosial telah menjadi bagian tak terpisahkan dari kehidupan modern...</p>
-            <a href="article3.html" class="read-more-button">Baca Selengkapnya</a>
+            <a href="article3.php" class="read-more-button">Baca Selengkapnya</a>
         </div>
     </div>
     <script src="script.js"></script>
